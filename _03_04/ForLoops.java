13d4da0da9663c975b9afefb8bf46cebee14377b
MihailescuCatalina <160478231+MihailescuCatalina@users.noreply.github.com>
2024-02-20 14:51:00 +0000

    End of day

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,18 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
+    for (int i=0; i<5; i++) {
+      System.out.println("I love you!");
+    }
     // Write a for loop that prints out the numbers 1 to 10
+    for (int a =1; a<=10; a++) {
+      System.out.println(a);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for (int d=10; d>=1; d--) {
+      System.out.println(d);
+    }
 
   }
 
